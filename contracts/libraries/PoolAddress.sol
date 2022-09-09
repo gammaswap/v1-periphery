054f509af13fb77833f9a27c275bf2d0125c643e
TBomberman <TBomberman@users.noreply.github.com>
2022-09-09 15:54:24 -0700

    Revert "Merge pull request #10 from gammaswap/tokens-hash"
    This reverts commit f7d5047a3b096fb5d472fd3888fa728258ee4f82, reversing
    changes made to 6daf6b8f5c34badbc98f330bb16d5f893e6a9e6f.

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0x157cb49461412afba53e7bd9359b3da3e81a31825666371966e5354af6fe2693;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0x44d02e958d6e2c08ac7280e9ca2a8e8cb343d473a754e6d4f83e830cb508c9cb;
 
     function getPoolKey(address cfmm, uint24 protocol) internal pure returns(bytes32 key) {
         key = keccak256(abi.encode(cfmm, protocol));
---
+++ scripts/deployPreCore.ts
@@ -0,0 +1,62 @@
+import { ethers } from "hardhat";
+const UniswapV2FactoryJSON = require("@uniswap/v2-core/build/UniswapV2Factory.json");
+import type { TestERC20 } from "../typechain/TestERC20";
+
+async function main() {
+  const [owner] = await ethers.getSigners();
+  const TestERC20Contract = await ethers.getContractFactory("TestERC20");
+  const tokenA = await TestERC20Contract.deploy("Test Token A", "TOKA");
+  const tokenB = await TestERC20Contract.deploy("Test Token B", "TOKB");
+  const tokenC = await TestERC20Contract.deploy("Test Token C", "TOKC");
+  const WETH = await TestERC20Contract.deploy("WETH", "WETH");
+  await tokenA.deployed();
+  await tokenB.deployed();
+  await tokenC.deployed();
+  await WETH.deployed();
+  console.log("tokenA Address >> " + tokenA.address);
+  console.log("tokenB Address >> " + tokenB.address);
+  console.log("tokenC Address >> " + tokenC.address);
+  console.log("WETH Address >> " + WETH.address);
+
+  const UniswapV2Factory = new ethers.ContractFactory(
+    UniswapV2FactoryJSON.abi,
+    UniswapV2FactoryJSON.bytecode,
+    owner
+  );
+
+  const uniFactory = await UniswapV2Factory.deploy(owner.address);
+  await uniFactory.deployed();
+  console.log("UniswapV2Factory Address >> " + uniFactory.address);
+
+  const UNI_COMPUTED_INIT_CODE_HASH = ethers.utils.keccak256(
+    UniswapV2Factory.bytecode
+  );
+  console.log("uni factory hash >> " + UNI_COMPUTED_INIT_CODE_HASH)
+
+  async function createPair(token1: TestERC20, token2: TestERC20) {
+    await uniFactory.createPair(token1.address, token2.address);
+    const uniPairAddress: string = await uniFactory.getPair(
+      token1.address,
+      token2.address
+    );
+    const token1Symbol = await token1.symbol();
+    const token2Symbol = await token2.symbol();
+    console.log(token1Symbol + "/" + token2Symbol + " uniPairAddress >> "
+      + uniPairAddress);
+    return uniPairAddress;
+  }
+
+  await createPair(tokenA, tokenB);
+  await createPair(tokenA, tokenC);
+  await createPair(tokenB, tokenC);
+  await createPair(tokenA, WETH);
+  await createPair(tokenB, WETH);
+  await createPair(tokenC, WETH);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
