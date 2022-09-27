729817480983684073a1ff356a71428b81bc54b4
TBomberman <TBomberman@users.noreply.github.com>
2022-09-26 21:39:22 -0700

    Remove hard coded address.

--- scripts/deployPostCore.ts
+++ scripts/deployPostCore.ts
@@ -1,10 +1,8 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  // const wethAddress = "<get this from deployPreCore deploy logs>";
-  // const GammaFactoryAddress = "<get this from v1core pre-strat deploy logs>";
-  const wethAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
-  const GammaFactoryAddress = "0x67d269191c92Caf3cD7723F116c85e6E9bf55933";
+  const wethAddress = "<get this from deployPreCore deploy logs>";
+  const GammaFactoryAddress = "<get this from v1core pre-strat deploy logs>";
 
   const PositionManager = await ethers.getContractFactory("PositionManager");
   const positionManager = await PositionManager.deploy(
