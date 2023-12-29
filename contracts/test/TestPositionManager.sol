e25f788cbe0ae0a68bff1926bddb49f51a7d53cb
Kalesberg <mobaddicts@hotmail.com>
2023-12-29 09:29:34 -0500

    recover missing funcs

--- contracts/test/TestPositionManager.sol
+++ contracts/test/TestPositionManager.sol
@@ -4,10 +4,17 @@ pragma solidity 0.8.21;
 import "../PositionManager.sol";
 
 contract TestPositionManager is PositionManager {
-
     function createTestLoan(address to) external virtual returns(uint256 tokenId) {
         tokenId = 1;
         _safeMint(to, tokenId);
+    }
+
+    function testCheckMinReserves(uint256[] calldata amounts, uint256[] calldata amountsMin) external virtual pure {
+        checkMinReserves(amounts, amountsMin);
+    }
+
+    function testCheckMinCollateral(uint128[] memory amounts, uint128[] memory amountsMin) external virtual pure {
+        checkMinCollateral(amounts, amountsMin);
     }
 
     function tokenURI(uint256) public view virtual override returns (string memory) {
